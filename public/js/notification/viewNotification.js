ea1ee6f7b72c79f0433020c2c5ef6c709cc783f8
K-Kyler <khaiquang690@gmail.com>
2021-03-14 20:20:47 +0700

    Finish CRUD for notification

---
+++ public/js/notification/viewNotification.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+    // Show view notification modal
+    $("body").on("click", ".viewNotification", (event) => {
+        let notificationId = $(event.target).data("notificationid");
+
+        event.preventDefault();
+        $("#viewNotificationButton").attr(
+            "data-notificationId",
+            notificationId
+        );
+        fetch(`/notification/${notificationId}`)
+            .then((response) => response.json())
+            .then((result) => {
+                if (result.code === 1) {
+                    $("#viewNotificationTitle").html(
+                        result.data.notificationName
+                    );
+                    $("#viewNotificationContent").html(
+                        result.data.notificationContent
+                    );
+                }
+            })
+            .catch((error) => console.log(error));
+        $("#viewNotificationModal").modal("toggle");
+    });
+});
