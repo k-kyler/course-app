4075e9f4b5f5874dc64aa469a44331f542769940
K-Kyler <khaiquang690@gmail.com>
2021-03-27 19:10:43 +0700

    Add teacher role

--- controllers/teacherDashboard.controller.js
+++ controllers/teacherDashboard.controller.js
@@ -1,5 +1,16 @@
+const User = require("../models/user.model");
+const Notification = require("../models/notification.model");
+
 // Notification
-module.exports.teacherNotification = async (req, res) => {};
+module.exports.teacherNotification = async (req, res) => {
+    let user = await User.findById(req.signedCookies.userId);
+    let notifications = await Notification.find();
+
+    res.render("dashboards/teacher/notification", {
+        user,
+        notifications,
+    });
+};
 
 // Schedule
 module.exports.teacherSchedule = async (req, res) => {};
