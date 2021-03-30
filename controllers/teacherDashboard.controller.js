662c2ec5499810ce2a089579f9a50abf790cc500
K-Kyler <khaiquang690@gmail.com>
2021-03-30 18:49:55 +0700

    Finish teacher functions

--- controllers/teacherDashboard.controller.js
+++ controllers/teacherDashboard.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const Notification = require("../models/notification.model");
+const LearningSchedule = require("../models/learningSchedule.model");
+const Course = require("../models/course.model");
 
 // Notification
 module.exports.teacherNotification = async (req, res) => {
@@ -13,4 +15,30 @@ module.exports.teacherNotification = async (req, res) => {
 };
 
 // Schedule
-module.exports.teacherSchedule = async (req, res) => {};
+module.exports.teacherSchedule = async (req, res) => {
+    let user = await User.findById(req.signedCookies.userId);
+    let learningSchedules = await LearningSchedule.find();
+    let courses = await Course.find();
+    let teacherLearningSchedules = [];
+
+    for (let course of courses) {
+        for (let learningSchedule of learningSchedules) {
+            if (
+                course.courseId === learningSchedule.courseId &&
+                course.courseTeacher === user.fullname
+            ) {
+                teacherLearningSchedules.push({
+                    courseName: course.courseName,
+                    room: learningSchedule.room,
+                    date: learningSchedule.date,
+                    time: learningSchedule.time,
+                });
+            }
+        }
+    }
+
+    res.render("dashboards/teacher/schedule", {
+        user,
+        teacherLearningSchedules,
+    });
+};
--- public/js/schedule/removeDuplicateSchedule.js
+++ public/js/schedule/removeDuplicateSchedule.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     let checkDuplicateLC = {};
     let checkDuplicateEC = {};
+    let checkDuplicateLCTeacher = {};
 
     $("#studentLearningScheduleList tr").each(function () {
         let item = $(this).text();
@@ -19,6 +20,16 @@ $(document).ready(() => {
             $(this).remove();
         } else {
             checkDuplicateEC[item] = true;
+        }
+    });
+
+    $("#teacherLearningScheduleList tr").each(function () {
+        let item = $(this).text();
+
+        if (checkDuplicateLCTeacher[item]) {
+            $(this).remove();
+        } else {
+            checkDuplicateLCTeacher[item] = true;
         }
     });
 });
