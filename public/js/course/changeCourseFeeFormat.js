e1411f575b21015944acf8dba041c03e14ebd729
K-Kyler <khaiquang690@gmail.com>
2021-04-01 22:16:51 +0700

    Deploy

--- public/js/course/changeCourseFeeFormat.js
+++ public/js/course/changeCourseFeeFormat.js
@@ -1,16 +1,24 @@
+// Convert course fee field to currency format
+document.getElementById("courseFee").value = Intl.NumberFormat().format(
+    courseFee
+);
+
 document.getElementById("courseFee").addEventListener("change", (event) => {
     let courseFee = Number(event.target.value.replace(/[^0-9.-]+/g, ""));
 
-    // Convert course fee field to currency format
     document.getElementById("courseFee").value = Intl.NumberFormat().format(
         courseFee
     );
 });
 
+// Convert edit course fee field to currency format
+document.getElementById("editCourseFee").value = Intl.NumberFormat().format(
+    editCourseFee
+);
+
 document.getElementById("editCourseFee").addEventListener("change", (event) => {
     let editCourseFee = Number(event.target.value.replace(/[^0-9.-]+/g, ""));
 
-    // Convert edit course fee field to currency format
     document.getElementById("editCourseFee").value = Intl.NumberFormat().format(
         editCourseFee
     );
