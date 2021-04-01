21fcfbde779c2300f16201e480e95d9ac5e46122
K-Kyler <khaiquang690@gmail.com>
2021-04-01 20:38:23 +0700

    Deploy

--- public/js/paymentProcess.js
+++ public/js/paymentProcess.js
@@ -24,7 +24,7 @@ $(document).ready(() => {
                         );
                         $("#paymentProcessContent").html(
                             result.message +
-                                "<a href='https://school-ibanking-app.herokuapp.com/'>Đến trang thanh toán</a>"
+                                "<a target='_blank' href='https://school-ibanking-app.herokuapp.com/'>Đến trang thanh toán</a>"
                         );
                     }, 800);
                 } else if (result.code === 0) {
